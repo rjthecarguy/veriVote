609113e430dc7df8b6fab9a0edf4e849c31c7e6e
RJ <rj@rjrobinson.net>
2025-06-21 17:05:21 -0700

    added user edit

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+
+    <div class="container">
+
+        <h2 class="text-3xl mt-4 ml-4">Edit User</h2>
+
+    </div>
+
+
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\roleAdmin;
@@ -11,6 +12,8 @@ Route::get('/', function () {
 });
 
 Route::get('/admin', [AdminController::class, 'index'])->middleware(['auth',roleAdmin::class])->name('admin');
+Route::get('/user/edit', [UserController::class, 'edit'])->middleware(['auth',roleAdmin::class])->name('user.admin');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
