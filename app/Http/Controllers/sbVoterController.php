fd10daa168df3b4aff2542cc6c9c157dc2f3fa11
RJ <rj@rjrobinson.net>
2025-06-24 10:48:27 -0700

    Added comments

--- app/Http/Controllers/sbVoterController.php
+++ app/Http/Controllers/sbVoterController.php
@@ -8,18 +8,17 @@ use App\Models\SbVoter;
 class sbVoterController extends Controller
 {
     public function search(Request $request)
-
 {
-    
-
+     // Start query
      $query = SbVoter::query();
 
- 
+     // Convert to upper case    
      $name_last = strtoupper($request->name_last);
      $name_first = strtoupper($request->name_first);
      $street = strtoupper($request->street);
      $city = strtoupper($request->city);
 
+    // Ensure strings have value, if so, add to query
 
     $query->when($name_last, function ($q, $value) {
         $q->where('name_last',  'like', '%' . $value . '%');
@@ -44,6 +43,8 @@ class sbVoterController extends Controller
         $q->where('city', 'like', '%' . $value . '%');
     });
 
+
+    // Execute query and get results
     $results = $query->get();
 
    
