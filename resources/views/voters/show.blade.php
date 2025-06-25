40e0ecbb396379819a2e2d0197af6675cfd64436
RJ <rj@rjrobinson.net>
2025-06-24 20:35:29 -0700

    Added info row

--- resources/views/voters/show.blade.php
+++ resources/views/voters/show.blade.php
@@ -36,6 +36,7 @@ table tr:nth-child(even) {
 }
 
 
+
 </style>
 
 <x-app-layout>
@@ -44,61 +45,40 @@ table tr:nth-child(even) {
 
         <h2 class="text-2xl font-bold mt-[30px] mb-4">San Bernardino Results</h2>
 
-        <table id="sbVoters" class="table table-striped w-[90%] border">
-            <thead>
-                <tr class="bg-blue-100">
-                    
-                    <th>Last Name</th>
-                    <th>First Name</th>
-                    <th>House Number</th>
-                    <th>Street</th>
-                    <th>City</th>
-                </tr>
-            </thead>
-            <tbody>
-    
-                @forelse($sbVoters as $sbVoter)
-                <tr class="hover:bg-blue-200">
-                   
-                    <td>{{$sbVoter->name_last}}</td>
-                    <td>{{$sbVoter->name_first}}</td>
-                    <td>{{$sbVoter->house_number}}</td>
-                    <td>{{$sbVoter->street}}</td>
-                    <td>{{$sbVoter->city}}</td>
-                     
-                </tr>
-              
-                
-                @empty
-    
-                @endforelse
-              
-            </tfoot>
-        </table>
+       
 
-    </div>
 
     <table class="table-auto w-full border-collapse border border-gray-300">
         <thead class="bg-gray-100">
             <tr>
-                <th class="border px-4 py-2">Expand</th>
-                <th class="border px-4 py-2">Name</th>
-                <th class="border px-4 py-2">Email</th>
+                <th class="border px-4 py-2"></th>
+                <th class="border px-4 py-2">Last Name</th>
+                <th class="border px-4 py-2">First Name</th>
+                <th class="border px-4 py-2">House Number</th>
+                <th class="border px-4 py-2">Street</th>
+                <th class="border px-4 py-2">City</th>
             </tr>
         </thead>
         <tbody>
-            @forelse ($sbVoters as $user)
-                <tr class="border-b user-row" data-user-id="{{ $user->id }}">
+            @forelse ($sbVoters as $voter)
+                <tr class="border-b user-row {{ $loop->odd ? 'bg-white' : 'bg-gray-100' }}" data-user-id="{{ $voter->id }}">
                     <td class="border px-4 py-2 text-center">
-                        <button class="toggle-row" data-id="{{ $user->id }}">▶</button>
+                        <button class="toggle-row" data-id="{{ $voter->id }}"><span class="text-green-500 text-2xl icon"></span></button>
                     </td>
-                    <td class="border px-4 py-2">{{ $user->name_last }}</td>
-                    <td class="border px-4 py-2">{{ $user->email }}</td>
+                    <td class="border px-4 py-2">{{ $voter->name_last }}</td>
+                    <td class="border px-4 py-2">{{ $voter->name_first }}</td>
+                    <td class="border px-4 py-2">{{ $voter->house_number }}</td>
+                    <td class="border px-4 py-2">{{ $voter->street }}</td>
+                    <td class="border px-4 py-2">{{ $voter->city}}</td>
                 </tr>
-                <tr class="details-row hidden" id="details-{{ $user->id }}">
+                <tr class="details-row hidden" id="details-{{ $voter->id }}">
                     <td colspan="3" class="px-4 py-2 bg-gray-100 text-sm text-gray-700">
                         <strong>Details:</strong><br>
-                        Role: {{ $user->city}}<br>
+                        Party: {{ $voter->party}}<br>
+                        Gender: {{ $voter->gender}}<br>
+                        Birth Date: {{ \Carbon\Carbon::parse($voter->birth_date)->format('Y-m-d') }}
+                        <br>
+                        Birth Place: {{ $voter->birth_place}}<br>
                       
                     </td>
                 </tr>
@@ -111,21 +91,24 @@ table tr:nth-child(even) {
             @endforelse
         </tbody>
     </table>
-
+</div>
     <script>
         $(document).ready(function () {
             $('.toggle-row').click(function () {
                 let id = $(this).data('id');
                 $('#details-' + id).toggle(); // toggle the details row
                 let icon = $(this).text();
-                $(this).text(icon === '▶' ? '▼' : '▶');
+                //$(this).text(icon === '▶' ? '▼' : '▶');
+                const $icon = $(this).find('.icon');
+
+                $icon.toggleClass('text-red-600');
             });
         });
     </script>
     
 
 <script>
-
+ $('.icon').html('<i class="fa-solid fa-circle-info"></i>');
   //new DataTable('#sbVoters');
 
 </script>
