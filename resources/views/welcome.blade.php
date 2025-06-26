2416f486934ecfed9aaa8f4b1133d3893493b710
RJ <rj@rjrobinson.net>
2025-06-25 20:07:28 -0700

    Added welcome page stuff

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
-            @if (Route::has('login'))
+           {{--  @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
                     @auth
                         <a
@@ -47,7 +47,7 @@
                         @endif
                     @endauth
                 </nav>
-            @endif
+            @endif --}}
         </header>
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
             <main class="">
@@ -65,8 +65,40 @@
                        
                         
                     </p>
-                   
-                    
+        <!-- Register - Login block -->
+        <div class="w-[40%] mx-auto bg-gray-100 rounded p-4 mt-5">
+
+                        @if (Route::has('login'))
+             
+                    @auth
+                  
+                        <a
+                            href="{{ url('/dashboard') }}"
+                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
+                        >
+                            Dashboard
+                        </a>
+             
+                    @else
+                        <a
+                            href="{{ route('login') }}"
+                            class="mb-4 md:mb-0 inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
+                        >
+                            Log in
+                        </a>
+
+                        @if (Route::has('register'))
+                            <a
+                                href="{{ route('register') }}"
+                                class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                                Register
+                            </a>
+                        @endif
+                    @endauth
+                
+            @endif
+
+    </div>  <!-- End of Register - login block -->
                    
                     
                 </div>
