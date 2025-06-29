5e95498a0256466035d6489858fe8e6ee6b89582
RJ <rj@rjrobinson.net>
2025-06-28 21:06:23 -0700

    question create

--- app/Models/SurveyOption.php
+++ app/Models/SurveyOption.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class SurveyOption extends Model
 {
-    protected $fillable = ['survey_question_id', 'option_text'];
+    protected $fillable = ['survey_question_id',
+                            'option_text',
+                            'question_text'];
 
     public function question()
     {
